78259641c16c66865539c7e17ebfb0ad0693caea
dpowell856 <56113042+dpowell856@users.noreply.github.com>
2020-01-29 13:44:20 +0000

    Basics for Player Movement

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public float moveSpeed = 2f; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,moveSpeed*Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
