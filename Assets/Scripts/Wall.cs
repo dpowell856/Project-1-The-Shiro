d0a778ccff1fa033dff5e1046d68cae9362b7a5c
lazyraysstudios <lazyraysstudios@gmail.com>
2020-01-30 08:37:32 +0000

    Walls Added

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Wall : MonoBehaviour
+{
+    [SerializeField] private float _selfDestructTime;
+    [SerializeField] private float _repairTime;
+    [SerializeField] private float _totalLife;
+
+    private float _life;
+
+    private TextMeshProUGUI _lifeText;
+
+    void Awake()
+    {
+        _lifeText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    void Start()
+    {
+        _life = _totalLife;
+    }
+
+    void Update()
+    {
+        ShowCurrentLife();
+    }
+
+    private void FixedUpdate()
+    {
+        _life -= _totalLife * (Time.fixedDeltaTime / _selfDestructTime);
+        if(_life <= 0)
+        {
+            Break();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _life += _totalLife * (Time.fixedDeltaTime / _repairTime);
+            if(_life > 100)
+            {
+                _life = 100;
+            }
+        }
+    }
+
+    private void ShowCurrentLife()
+    {
+        _lifeText.text = Mathf.CeilToInt(_life) + " / " + _totalLife;
+    }
+
+    private void Break()
+    {
+        Destroy(gameObject);
+    }
+}
