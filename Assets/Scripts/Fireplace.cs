df91fff702f3cac1b194bb6dc26e7636a78e8387
HVWijnschenk <56162226+HVWijnschenk@users.noreply.github.com>
2020-02-08 15:43:26 +0000

    Add files via upload

--- Assets/Scripts/Fireplace.cs
+++ Assets/Scripts/Fireplace.cs
@@ -5,29 +5,62 @@ using UnityEngine;
 public class Fireplace : MonoBehaviour
 {
     private bool _lit = false;
-
     private bool _active;
+
+    private float _timeLeft = 2.0f;
 
     private SpriteRenderer _spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        float _random = Random.Range(0.0f, 2.0f);
-
-        _active = _random < 1.0f;
-
         _spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (!_active){
+            _spriteRenderer.color = new Color(0, 0, 0);
+        } else if (_active && !_lit)
+        {
+            _spriteRenderer.color = new Color(255, 255, 255);
+        } else
+        {
+            _spriteRenderer.color = new Color(255, 0, 0);
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            if (collision.GetComponent<Fighter>().player.GetAction(Action.Interact))
+            if(_timeLeft > 0)
             {
-                print("Player is inside torch usable zone");
+                _timeLeft -= Time.deltaTime;
+            } else {
+                _lit = true;
             }
         }
+    }
+
+    public bool isActive()
+    {
+        return _lit;
+    }
+
+    public bool isDone()
+    {
+        if (_lit || !_active)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    public void setActive(bool activate)
+    {
+        _active = activate;
     }
 }
---
+++ Assets/Scripts/Fireplaces.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireplaces : MonoBehaviour
+{
+    private Fireplace[] _fireplaces;
+    private int _wave = 0;
+
+    void Start()
+    {
+        _fireplaces = GetComponentsInChildren<Fireplace>();
+    }
+
+    void Update()
+    {
+        if (IsAllActiveFireplacesLit() || _wave == 0)
+        {
+            StartNextWave();
+        }
+    }
+
+    private bool IsAllActiveFireplacesLit()
+    {
+        bool allActive = true;
+        for(int i = 0; i < _fireplaces.Length; i++)
+        {
+            if (_fireplaces[i].isDone() == false){ allActive = false; }    
+        }
+
+        return allActive;
+        
+        //loop through activated fireplaces and check if their all lit return true
+    }
+
+    private void StartNextWave()
+    {
+        for (int i = 0; i < _fireplaces.Length; i++)
+        {
+            _fireplaces[i].setActive(false);
+        }
+
+        int _random = Random.Range(4, 7);
+        Debug.Log("Amount: " + _random);        
+
+        for (int i = 0; i < _random; i++)
+        {
+            int _activateFire = Random.Range(0, _fireplaces.Length);
+            Debug.Log("Random " +_activateFire);
+            while (_fireplaces[_activateFire].isActive() == true)
+            {
+                _activateFire = Random.Range(0, _fireplaces.Length);
+                Debug.Log("Repeat: " + _activateFire);
+            }
+            _fireplaces[_activateFire].setActive(true);
+        }
+        _wave += 1;
+    }
+}
