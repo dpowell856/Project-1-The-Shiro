b5433e8ea99ff0da7dfcb9d49bd0601b537d764d
Daniel-Holmes1 <U1962485@unimail.hud.ac.uk>
2020-02-12 14:23:41 +0000

    Made catapult projectile move

--- Assets/Scripts/CatapultProjectile.cs
+++ Assets/Scripts/CatapultProjectile.cs
@@ -7,6 +7,7 @@ public class CatapultProjectile : Damageable
     // Start is called before the first frame update
     void Start() {
         
+        _health = _totalHealth;
     }
 
     // Fixed update is called once per fixed interval
